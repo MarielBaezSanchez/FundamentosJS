e896e2ddf3d1b4b475c077389958663ccf7e7be8
KarenAlvere <alvarezpalafoxanakaren@gmail.com>
2023-09-11 11:35:48 -0600

    02 Tipos de datos en JS 03 Variables

---
+++ variables.js
@@ -0,0 +1,24 @@
+// versiones anteriores a ES6 era var
+var miVarieble;
+
+//de ES6 en adelante
+// const y let
+
+//const declara constatente, no pemite reasignacion
+// al crearla se debe inicializar
+const PI = 3.1416;
+// PI = 3.1416;
+
+//let declara variables
+//petmite reasignacion
+//se puede declarar y luego inicializar
+//JS detecta automaticamete el tipo de datos
+let firstname;
+console.log(firstname);
+firstname = 'Karen Alvarez';
+console.log(firstname);
+console.log(typeof(firstname))
+
+firstname= 18;
+console.log(firstname);
+console.log(typeof(firstname));
