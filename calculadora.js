60fea99dd76eb8d35eb61e52fc001839005a4ee7
Mariel Baez <marielbaezsanchez991@gmail.com>
2023-09-13 12:45:22 -0600

    07 Mejorando Calculadora

--- calculadora.js
+++ calculadora.js
@@ -29,3 +29,8 @@ const limpiar = () => {
     num2.value = '';
     result.value = '';
 }
+
+const random = () => {
+    num1.value = Math.round(Math.random() * 100000);
+    num2.value = Math.round(Math.random() * 100000);
+}
